9d93eb35503009f07bd2ed850f7a9b969245770e
Himalayastechies <Himalayastechies@gmail.com>
2026-04-29 21:24:22 +0530

    connect frontend to live backend

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 require('dotenv').config();
 
 
-app.use(cors({
-  origin: "*"
-}));
+
 console.log("ENV CHECK:", process.env.MONGO_URI);
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+  origin: "*"
+}));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.log("❌ MongoDB connection error:", err));
